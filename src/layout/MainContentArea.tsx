e99ee3f79ad703f72651a07013c3ed4f7ecb223f
laorozcoe <laorozcoe@gmail.com>
2026-01-30 21:35:42 -0600

    wrapper de usuario

---
+++ src/layout/MainContentArea.tsx
@@ -0,0 +1,30 @@
+'use client'
+import AppSidebar from "@/layout/AppSidebar";
+import Backdrop from "@/layout/Backdrop";
+import { useSidebar } from "@/context/SidebarContext";
+import AppHeader from "@/layout/AppHeader";
+
+const MainContentArea = ({ children }: { children: React.ReactNode }) => {
+    const { isExpanded, isHovered, isMobileOpen } = useSidebar();
+    const mainContentMargin = isMobileOpen
+        ? "ml-0"
+        : isExpanded || isHovered
+            ? "lg:ml-[290px]"
+            : "lg:ml-[90px]";
+    return (
+        <>
+            <AppSidebar />
+            <Backdrop />
+            <div
+                className={`flex-1 transition-all  duration-300 ease-in-out ${mainContentMargin}`}
+            >
+                {/* Header */}
+                <AppHeader />
+                {/* Page Content */}
+                <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
+            </div>
+        </>
+    );
+};
+
+export default MainContentArea;
