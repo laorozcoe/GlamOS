908f99a3b517012eee6bc1b5b81e9c476f88045f
laorozcoe <laorozcoe@gmail.com>
2026-02-25 17:45:13 -0600

    que pedo cuando se desmadro esto

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,6 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
         short_name: 'Brillarte',
         description: 'Sistema de agenda y punto de venta',
         start_url: '/',
+        scope: '/', // <--- ESTA ES LA MAGIA PARA LAS REDIRECCIONES
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#ffffff',
