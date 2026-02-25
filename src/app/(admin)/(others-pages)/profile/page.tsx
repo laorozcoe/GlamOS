e8214faec638caf08570fd14c7f87ec8c5a24c2f
laorozcoe <laorozcoe@gmail.com>
2026-02-25 17:17:26 -0600

    plz work xD

--- src/app/(admin)/(others-pages)/profile/page.tsx
+++ src/app/(admin)/(others-pages)/profile/page.tsx
@@ -5,7 +5,7 @@ import UserMetaCard from "@/components/user-profile/UserMetaCard";
 export default function Profile() {
   return (
     <div>
-      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/3 lg:p-6">
         <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
           Profile
         </h3>
