47dad75bfd342ae838c4e1d9d82f1eeecc7678dd
laorozcoe <laorozcoe@gmail.com>
2026-04-11 20:02:45 -0600

    flx work

--- src/app/(admin)/(others-pages)/employees/EmployeeClient.tsx
+++ src/app/(admin)/(others-pages)/employees/EmployeeClient.tsx
@@ -193,6 +193,7 @@ export default function EmployeeClient({ users }: EmployeeClientProps) {
                 onChange={handleChange}
                 placeholder="Juan"
 
+
               />
             </div>
             <div>
